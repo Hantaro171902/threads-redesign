ae7128a88f29d6907c7f7711d18e2f6cd752e23f
Hantaro191702 <chaugiahan001@gmail.com>
2024-11-19 21:25:21 +0700

    update

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,19 @@
+export const signup = async (res, req) => {
+    try {
+
+    } catch (error) {
+        
+    }
+};
+
+export const login = async (res, req) => {
+  res.json({
+    data: "You hit the login endpoint",
+  });
+};
+
+export const logout = async (res, req) => {
+  res.json({
+    data: "You hit the logout endpoint",
+  });
+};
