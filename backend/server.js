f58dde13009c0d659230b80a66f4911db9c5bf6d
Hantaro191702 <chaugiahan001@gmail.com>
2024-11-19 21:36:51 +0700

    1-project setup

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ console.log(process.env.MONGO_URI);
 
 app.use("api/auth", authRoutes);
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectMongoDB();
 });
