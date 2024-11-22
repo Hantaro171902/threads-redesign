9611815b595e4be9df75a978b472554e6e8bb4a5
Hantaro191702 <chaugiahan001@gmail.com>
2024-11-22 23:59:15 +0700

    3- Auth handled and routes protected in client

--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-import XSvg from "../../../components/svgs/X";
+import PLUSSvg from "../../../components/svgs/PLUS";
 
 import { MdOutlineMail } from "react-icons/md";
 import { MdPassword } from "react-icons/md";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 const LoginPage = () => {
   const [formData, setFormData] = useState({
@@ -12,8 +14,44 @@ const LoginPage = () => {
     password: "",
   });
 
+  const querryClient = useQueryClient();
+  const {
+    mutate: loginMutation,
+    isError,
+    isPending,
+    error,
+  } = useMutation({
+    mutationFn: async ({ username, password }) => {
+      try {
+        const res = await fetch("api/auth/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, password }),
+        });
+        const data = await res.json();
+
+        if (!res.ok) throw new Error(data.error || "Something went wrong");
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+    },
+    onError: () => {
+      toast.error("Failed to login");
+    },
+    onSuccess: () => {
+      //  refetch the authUser
+      querryClient.invalidateQueries({ queryKey: ["authUser"] });
+      //   toast.success("Login successfully");
+    },
+  });
+
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault(); //  page won't reload
+    loginMutation(formData);
     console.log(formData);
   };
 
@@ -21,16 +59,14 @@ const LoginPage = () => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const isError = false;
-
   return (
     <div className="max-w-screen-xl mx-auto flex h-screen">
       <div className="flex-1 hidden lg:flex items-center  justify-center">
-        <XSvg className="lg:w-2/3 fill-white" />
+        <PLUSSvg className="lg:w-2/3 fill-white" />
       </div>
       <div className="flex-1 flex flex-col justify-center items-center">
         <form className="flex gap-4 flex-col" onSubmit={handleSubmit}>
-          <XSvg className="w-24 lg:hidden fill-white" />
+          <PLUSSvg className="w-24 lg:hidden fill-white" />
           <h1 className="text-4xl font-extrabold text-white">{"Let's"} go.</h1>
           <label className="input input-bordered rounded flex items-center gap-2">
             <MdOutlineMail />
@@ -56,9 +92,9 @@ const LoginPage = () => {
             />
           </label>
           <button className="btn rounded-full btn-primary text-white">
-            Login
+            {isPending ? "Loading..." : "Login"}
           </button>
-          {isError && <p className="text-red-500">Something went wrong</p>}
+          {isError && <p className="text-red-500">{error.message}</p>}
         </form>
         <div className="flex flex-col gap-2 mt-4">
           <p className="text-white text-lg">{"Don't"} have an account?</p>
